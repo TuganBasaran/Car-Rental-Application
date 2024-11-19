db9abd0ad26ed8b5be9d7fe9dc71ea443b3903ea
sedenmutaf1 <seden.mutaf@ozu.edu.tr>
2024-11-19 17:01:32 +0300

    Controller bodies are created.

--- src/main/java/cs_393_TZS/car_rental_application/controller/CarController.java
+++ src/main/java/cs_393_TZS/car_rental_application/controller/CarController.java
@@ -1,4 +1,29 @@
 package cs_393_TZS.car_rental_application.controller;
 
+import cs_393_TZS.car_rental_application.model.Car;
+import cs_393_TZS.car_rental_application.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cars")
 public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping("/available")
+    public ResponseEntity<List<Car>> searchAvailableCars(
+            @RequestParam String carType,
+            @RequestParam String transmissionType) {
+        List<Car> availableCars = carService.searchAvailableCars(carType, transmissionType);
+        if (availableCars.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(availableCars, HttpStatus.OK);
+    }
 }
---
+++ src/main/java/cs_393_TZS/car_rental_application/controller/ReservationController.java
@@ -0,0 +1,18 @@
+package cs_393_TZS.car_rental_application.controller;
+
+import cs_393_TZS.car_rental_application.dto.ReservationRequest;
+import cs_393_TZS.car_rental_application.dto.ReservationResponse;
+import cs_393_TZS.car_rental_application.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    
+}
