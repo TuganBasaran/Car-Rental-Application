b00b3b359a9da34031e91ec956b2962b75051e84
sedenmutaf1 <seden.mutaf@ozu.edu.tr>
2024-11-19 17:01:10 +0300

    Annotation added.

--- src/main/java/cs_393_TZS/car_rental_application/CarRentalApplication.java
+++ src/main/java/cs_393_TZS/car_rental_application/CarRentalApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories("cs_393_TZS.car_rental_application.repository")
+@ComponentScan(basePackages = "cs_393_TZS.car_rental_application")
 public class CarRentalApplication {
 
 	public static void main(String[] args) {
