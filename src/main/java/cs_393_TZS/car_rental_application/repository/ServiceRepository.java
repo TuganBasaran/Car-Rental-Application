f9ede454ab4c070901e7cd01cb677f41fa11f11c
Tugan_Basaran <64231122+TuganBasaran@users.noreply.github.com>
2024-11-30 23:17:18 +0300

    Commit Test

--- src/main/java/cs_393_TZS/car_rental_application/repository/ServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package cs_393_TZS.car_rental_application.repository;
-
-import cs_393_TZS.car_rental_application.model.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Long> {
-}
